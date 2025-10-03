c8210cef46d2fc3974e52ae32bf15f4a1a0403fb
Oleg Parhomenko <shocked194@gmail.com>
2025-10-03 15:39:19 +0300

    5.2 'добавил валидацию с помощью Pydantic'

--- tests/api/test_auth_api.py
+++ tests/api/test_auth_api.py
@@ -14,14 +14,12 @@ class TestAuthAPI:
         assert register_user_response.email == test_user.email, "Email не совпадает"
 
     def test_register_and_login_user(self, api_manager: ApiManager, test_user: TestUser):
-        # Преобразуем Pydantic-модель в dict
+
         user_data = test_user.model_dump(mode="json")
 
-        # Сначала регистрируем пользователя
         register_response = api_manager.auth_api.register_user(user_data)
         assert register_response.status_code in [200, 201]
 
-        # Затем логинимся
         login_data = {
             "email": user_data["email"],
             "password": user_data["password"]
@@ -29,6 +27,5 @@ class TestAuthAPI:
         login_response = api_manager.auth_api.login_user(login_data)
         login_response_data = login_response.json()
 
-        # Проверки
         assert "accessToken" in login_response_data
         assert login_response_data["user"]["email"] == user_data["email"]
--- tests/api/test_user_api.py
+++ tests/api/test_user_api.py
@@ -1,23 +1,28 @@
+from utils.user_with_pydantic import TestUser, RegisterUserResponse
 
-class TestUser:
+
+class TestUserAPI:
 
     def test_create_user(self, super_admin, creation_user_data):
-        response = super_admin.api.user_api.create_user(creation_user_data).json()
 
-        assert response.get('id') and response['id'] != '', "ID должен быть не пустым"
-        assert response.get('email') == creation_user_data['email']
-        assert response.get('fullName') == creation_user_data['fullName']
-        assert response.get('roles', []) == creation_user_data['roles']
-        assert response.get('verified') is True
+        user_data = TestUser(**creation_user_data)
+        response = super_admin.api.user_api.create_user(user_data).json()
+
+        RegisterUserResponse(**response)
+
 
     def test_get_user_by_locator(self, super_admin, creation_user_data):
-        created_user_response = super_admin.api.user_api.create_user(creation_user_data).json()
-        response_by_id = super_admin.api.user_api.get_user(created_user_response['id']).json()
-        response_by_email = super_admin.api.user_api.get_user(creation_user_data['email']).json()
+        user_to_create  = TestUser(**creation_user_data)
+        created_user_response = super_admin.api.user_api.create_user(user_to_create ).json()
+
+        RegisterUserResponse(**created_user_response)
+
+        response_by_id = RegisterUserResponse(**super_admin.api.user_api.get_user(created_user_response['id']).json())
+        response_by_email = RegisterUserResponse(**super_admin.api.user_api.get_user(creation_user_data['email']).json())
 
         assert response_by_id == response_by_email, "Содержание ответов должно быть идентичным"
-        assert response_by_id.get('id') and response_by_id['id'] != '', "ID должен быть не пустым"
-        assert response_by_id.get('email') == creation_user_data['email']
-        assert response_by_id.get('fullName') == creation_user_data['fullName']
-        assert response_by_id.get('roles', []) == creation_user_data['roles']
-        assert response_by_id.get('verified') is True
+        assert response_by_id.id and response_by_id.id != '', "ID должен быть не пустым"
+        assert response_by_id.email == user_to_create.email
+        assert response_by_id.fullName == user_to_create.fullName
+        assert response_by_id.roles == user_to_create.roles
+        assert response_by_id.verified is True
